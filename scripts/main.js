84d14e8a1c6514625ee6b25d90abb5c8de293ce5
Banty Jha <bantyjha455@gmail.com>
2024-09-16 19:46:02 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   Banty Jha
     
     *****************
 */
@@ -94,7 +94,7 @@ function runalgo()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-    Banty Jha
     
     *****************
 */
